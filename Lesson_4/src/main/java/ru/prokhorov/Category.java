f3a8e1506930922fc2a9f4c0e7e5ad5f097f62be
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-02-13 11:00:42 +0300

    Add class Category

---
+++ Lesson_4/src/main/java/ru/prokhorov/Category.java
@@ -0,0 +1,32 @@
+package ru.prokhorov;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
