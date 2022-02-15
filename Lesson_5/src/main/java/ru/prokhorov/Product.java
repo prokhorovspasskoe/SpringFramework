4affc269aec395c27f1ff28303314201e379ba20
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-02-15 17:17:32 +0300

    Add class Product

---
+++ Lesson_5/src/main/java/ru/prokhorov/Product.java
@@ -0,0 +1,31 @@
+package ru.prokhorov;
+
+public class Product {
+    private int id;
+    private String title;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
