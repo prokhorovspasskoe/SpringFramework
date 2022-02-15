df87674b8b098420b12388e359a36f3c8dda6d24
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-02-15 17:54:10 +0300

    Add config

---
+++ Lesson_5/src/main/java/ru/prokhorov/Main.java
@@ -0,0 +1,7 @@
+package ru.prokhorov;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lesson_5/src/main/java/ru/prokhorov/ProductDao.java
@@ -0,0 +1,22 @@
+package ru.prokhorov;
+
+import java.util.List;
+
+public class ProductDao {
+
+    public Product findById(int id){
+        return null;
+    }
+
+    public List<Product> findAll(){
+        return null;
+    }
+
+    public void deleteById(int id){
+
+    }
+
+    public Product saveOrUpdate(Product product){
+        return null;
+    }
+}
