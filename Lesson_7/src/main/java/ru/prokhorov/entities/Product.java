35774dbadf33b16845c4edd1d7e97f8c285e5eb7
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-02-23 09:44:42 +0300

    Add class RestController

--- Lesson_7/src/main/java/ru/prokhorov/entities/Product.java
+++ Lesson_7/src/main/java/ru/prokhorov/entities/Product.java
@@ -5,12 +5,6 @@ public class Product {
     private String title;
     private int price;
 
-    public Product(int id, String title, int price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
-
     public int getId() {
         return id;
     }
