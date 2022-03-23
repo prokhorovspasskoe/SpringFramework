f378c48df7468e804274eaa9026e594635f83b72
Prokhorov <prokhorovspasskoe@yandex.ru>
2022-03-23 16:07:07 +0300

    Revert "Basket"
    This reverts commit e68e5654b69683b486f86044d17dfa20f7b0f229.

--- Shop/src/main/java/ru/prokhorov/launcher/SecurityConfig.java
+++ Shop/src/main/java/ru/prokhorov/launcher/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.prokhorov.config;
+package ru.prokhorov.launcher;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
